20f3ed7575c2afc06ecbe01edda90f3ad6c3d226
Brian Furlong <furlong@spstrutnov.cz>
2023-02-11 13:18:25 +0100

    Oprava bugu
    Špatná odpověď byla sprvná a to mi nevychaza.

--- src/citaty/app/controllers/hlaskyController.js
+++ src/citaty/app/controllers/hlaskyController.js
@@ -1,11 +1,12 @@
 const model = require('../models/hlaskyModel');
 
 exports.randomUcitel = (request, response) => {
+    let randomSeznamUcitelu = model.randomUcitel();
     response.render('hlasky/random', {
         // spravnaHlaska, spatneUcitele, ucitel
-        hlaska: model.randomUcitel()[0],
-        spravny: model.randomUcitel()[2],
-        spatny: model.randomUcitel()[1],
+        hlaska: randomSeznamUcitelu[0],
+        spravny: randomSeznamUcitelu[2],
+        spatny: randomSeznamUcitelu[1],
 
         //titulek: 'Registrace',
     });
--- src/citaty/app/models/hlaskyModel.js
+++ src/citaty/app/models/hlaskyModel.js
@@ -9,18 +9,20 @@ exports.randomUcitel = () => {
 
     let randomCislo = Math.round(Math.random() * length);
 
-    let ucitel = Object.keys(hlasky)[randomCislo];
-
-    let spravnaHlaska = hlasky[ucitel]["hlasky"][Math.round(Math.random() * (hlasky[ucitel]["hlasky"].length - 1))];
-
+    let spravnyUcitel = Object.keys(hlasky)[randomCislo];
+    
+    let spravnaHlaska = hlasky[spravnyUcitel]["hlasky"][Math.round(Math.random() * (hlasky[spravnyUcitel]["hlasky"].length - 1))];
+    console.log(spravnyUcitel);
+    console.log(spravnaHlaska);
+    
 
     let spatneUcitele = [];
 
-    for (let i = 1; i < 3;i++){
+    for (let i = 0; i < 3;i++){
         let spatnyUcitel = Object.keys(hlasky)[Math.round(Math.random() * length)];
         spatneUcitele.push(spatnyUcitel);
         
     }
     
-    return [spravnaHlaska, spatneUcitele, ucitel];
+    return [spravnaHlaska, spatneUcitele, spravnyUcitel];
 }
