649c5acb7bca5fb30f626b0962eb50a64d1da59f
Brian Furlong <furlong@spstrutnov.cz>
2023-02-11 14:29:27 +0100

    přidání počítání score a restartu kvízu po špatné odpovědi.
    Naschval zůstává stejná hláška po smrti aby hráč mohl sám zíjstit správnou odpověď místo toho aby se mu vypsala.

--- src/citaty/app/controllers/hlaskyController.js
+++ src/citaty/app/controllers/hlaskyController.js
@@ -1,8 +1,14 @@
 const model = require('../models/hlaskyModel');
 
 let randomSeznamUcitelu = model.randomUcitel();
+let score = 0;
 
 exports.randomUcitel = (req, res) => {
+
+    if (req.query.new == true){
+        score = 0;
+    }
+
     if (req.query.ucitel == undefined || req.query.hlaska == undefined){
         randomSeznamUcitelu = model.randomUcitel();
         return res.render('hlasky/random', {
@@ -11,25 +17,37 @@ exports.randomUcitel = (req, res) => {
             spravny: randomSeznamUcitelu[2],
             spatny: randomSeznamUcitelu[1],
             odpoved: "",
+            score: score,
         });
     }
     
-    if (model.checkOdpoved(req.query.ucitel, req.query.hlaska)){ //pokud zodpověděl zprávně:
+    if (model.checkOdpoved(req.query.ucitel, req.query.hlaska)){ // pokud zodpověděl zprávně:
+
         randomSeznamUcitelu = model.randomUcitel();
+        score += 1;
+
         return res.render('hlasky/random', {
             // spravnaHlaska, spatneUcitele, ucitel
             hlaska: randomSeznamUcitelu[0],
             spravny: randomSeznamUcitelu[2],
             spatny: randomSeznamUcitelu[1],
+            score: score,
             odpoved: "Správně",
         });
     }
     else{
-        return res.render('hlasky/random', {
+
+        // sem příjde logika pro ukládání nejvyžšího score
+        // až budou hotové profily.
+        
+        score = 0;
+
+        return res.render('hlasky/random', { // pokud nezodpověděl zprávně:
             // spravnaHlaska, spatneUcitele, ucitel
             hlaska: randomSeznamUcitelu[0],
             spravny: randomSeznamUcitelu[2],
             spatny: randomSeznamUcitelu[1],
+            score: score,
             odpoved: "Špatně",
         });
     }
