1015694c9118844267d6a8d2af3a96502a0ed4ab
Brian Furlong <furlong@spstrutnov.cz>
2023-04-08 14:35:12 +0200

    Oprava bugu.
    Mohlo se stát že v odpověďích na hlášku jsou rozdílnné gendery.

--- src/citaty/conf/index.js
+++ src/citaty/conf/index.js
@@ -2,5 +2,5 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-exports.port = process.env.PORT;
-exports.secret = process.env.SECRET;
+exports.port = process.env.PORT || 8000;
+exports.secret = process.env.SECRET || "pisscret974947970213012302312398213";
