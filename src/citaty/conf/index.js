aca16fec15aaa0f9aefc203f2201cfb00168d601
Brian Furlong <furlong@spstrutnov.cz>
2023-06-03 14:52:38 +0200

    Oprava

--- src/citaty/conf/index.js
+++ src/citaty/conf/index.js
@@ -3,9 +3,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 exports.port = process.env.PORT || 8000;
-exports.secret = process.env.SECRET || "pisscret974947970213012302312398213";
+exports.secret = process.env.SECRET;
 
 
 exports.casVyprseniKodu = 3; // v minutách
 exports.defaultJmeno = "Přihlásit se";
-exports.mailApiHeslo = process.env.APIHESLO || "fkmavpsvlubwpuxu"; // heslo pro API je v discordu
+exports.mailApiHeslo = process.env.APIHESLO || "kleslo"; // heslo pro API je v discordu
